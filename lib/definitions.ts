52b7d78dd457c0c6b587774fbcae343d992d26d7
Namika <loud.music.n@gmail.com>
2025-03-30 20:03:47 +0900

    - 暗記学習画面作成

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -4,3 +4,13 @@ export type Category = {
   count: number,
   pin: boolean,
 }
+
+export type Item = {
+  id: string,
+  category_id: string,
+  title: string,
+  answer: string,
+  created_at: Date,
+  updated_at: Date,
+  count: number,
+}
